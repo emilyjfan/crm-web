88dcc4a54d90194d3189dc43b55646a62d003071
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-28 22:05:43 -0500

    added sections to View All Contacts page

--- crm.rb
+++ crm.rb
@@ -3,22 +3,29 @@ require './rolodex'
 require 'sinatra'
 
 $rolodex = Rolodex.new
+@crm_app_name = "EMILY'S CRM APP"
 
 get '/' do 
-	@crm_app_name = "Emily's CRM App"
+	@crm_app_name = "EMILY'S CRM APP" 
 	erb :index
 end 
 
 get '/contacts' do
- 	@crm_app_name = "Emily's CRM App"
+ 	@crm_app_name = "EMILY'S CRM APP"
  	erb :contacts
 end
 
 
 get '/contacts/new' do 
-  @crm_app_name = "Emily's CRM App"
+  @crm_app_name = "EMILY'S CRM APP" 
   erb :new_contact
 end 
+
+#figure out how to view one contact only 
+get '/contacts/:name' do
+ 	@crm_app_name = "EMILY'S CRM APP"
+ 	erb :view_contact
+end
 
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
