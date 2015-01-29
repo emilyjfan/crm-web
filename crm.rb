c013cceb2a9c4b3629a020c793ace72f9971130b
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-29 16:07:24 -0500

    fixed ability to edit a contact

--- crm.rb
+++ crm.rb
@@ -49,8 +49,8 @@ get "/contacts/:id" do
 	end 
 end
 
-get "contacts/:id/edit" do 
-	@contact = @@rolodex.find(params[:id].to_i)
+get "/contacts/:id/edit" do 
+	@contact = @@rolodex.find_contact(params[:id].to_i)
 	if @contact
 		erb :edit_contact 
 	else
@@ -58,8 +58,19 @@ get "contacts/:id/edit" do
 	end
 end
 
+put "/contacts/:id" do
+	@contact = @@rolodex.find_contact(params[:id].to_i)
+	if @contact
+		@contact.first_name = params[:first_name]
+		@contact.last_name = params[:last_name]
+		@contact.email = params[:email]	
+		@contact.note = params[:note]
 
-
+		redirect to ("/contacts")	
+	else
+		raise Sinatra::NotFound 
+	end 
+end
 
 
 
