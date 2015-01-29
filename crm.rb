57ea5d9a5df6bb4f28af3898fcb894b6d13f0990
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-29 14:36:03 -0500

    added show_contact

--- crm.rb
+++ crm.rb
@@ -1,34 +1,40 @@
 require './contact'
 require './rolodex'
 require 'sinatra'
+require 'sinatra/reloader'
 
-$rolodex = Rolodex.new
-@crm_app_name = "EMILY'S CRM APP"
+@@rolodex = Rolodex.new
+@@rolodex.new_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
+
+@@crm_app_name = "EMILY'S CRM APP"
+
 
 get '/' do 
-	@crm_app_name = "EMILY'S CRM APP" 
 	erb :index
 end 
 
 get '/contacts' do
- 	@crm_app_name = "EMILY'S CRM APP"
  	erb :contacts
 end
 
 
 get '/contacts/new' do 
-  @crm_app_name = "EMILY'S CRM APP" 
   erb :new_contact
 end 
 
-#figure out how to view one contact only 
-get '/contacts/:name' do
- 	@crm_app_name = "EMILY'S CRM APP"
- 	erb :view_contact
-end
+# #figure out how to view one contact only 
+# get '/contacts/:name' do
+#  	erb :view_contact
+# end
 
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.new_contact(new_contact)
   redirect to('/contacts')
+end
+
+get "/contacts/1000" do
+  @contact = @@rolodex.find(1000)
+  erb :show_contact
 end
